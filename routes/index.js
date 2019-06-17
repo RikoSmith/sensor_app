afb949a98743adbd3977f8a91182013902a9cc9a
Riko <raimbek.riko@gmail.com>
2019-06-17 15:16:04 +0600

    added sensor

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: req.app.get("temp_server") });
+  console.log(req.rawHeaders);
+});
+
+router.get("/s/server_room", function(req, res, next) {
+  console.log(req.query);
+  var txt =
+    "Температура серверной: " +
+    req.query.temp_c +
+    String.fromCharCode(176) +
+    "C";
+  req.app.set("temp_server", txt);
+  res.send("ok");
 });
 
 module.exports = router;
