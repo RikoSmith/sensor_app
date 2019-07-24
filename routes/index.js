78aba2ea89e323741c7c45ed0166b54cc21a0d1a
Riko <raimbek.riko@gmail.com>
2019-07-24 10:41:52 +0600

    added dance room temp

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", {
-    title: req.app.get("temp_server") + " || " + req.app.get("temp_hall")
+    title:
+      req.app.get("temp_server") +
+      " || " +
+      req.app.get("temp_hall") +
+      " || " +
+      req.app.get("temp_droom")
   });
   console.log(req.rawHeaders);
 });
--- routes/sensors.js
+++ routes/sensors.js
@@ -24,10 +24,10 @@ router.get("/server_room", function(req, res, next) {
     MyDate.getFullYear();
 
   var txt =
-    "Температура в серверной: " +
+    "Серверная: " +
     req.query.temp_c +
     String.fromCharCode(176) +
-    "C  (посл. обновление: " +
+    "C  (посл. обн.: " +
     MyDateString +
     ")";
 
@@ -73,10 +73,10 @@ router.get("/hall", function(req, res, next) {
     MyDate.getFullYear();
 
   var txt =
-    "Температура в зале: " +
+    "Зал: " +
     req.query.temp_c +
     String.fromCharCode(176) +
-    "C  (посл. обновление: " +
+    "C  (посл. обн.: " +
     MyDateString +
     ")";
 
@@ -94,6 +94,55 @@ router.get("/hall", function(req, res, next) {
     }
     a.push({ temp: req.query.temp_c, timestamp: MyDateString });
     db.updateItem("hall", a);
+    //console.log("After: ");
+    //console.log(a);
+    res.send("ok");
+  });
+});
+
+//from dance room's temperature sensor
+router.get("/droom", function(req, res, next) {
+  //console.log(req.query);
+  var MyDate = new Date();
+  var MyDateString;
+
+  MyDate.setDate(MyDate.getDate());
+
+  MyDateString =
+    ("0" + MyDate.getHours()).slice(-2) +
+    ":" +
+    ("0" + MyDate.getMinutes()).slice(-2) +
+    ":" +
+    ("0" + MyDate.getSeconds()).slice(-2) +
+    " " +
+    ("0" + MyDate.getDate()).slice(-2) +
+    "/" +
+    ("0" + (MyDate.getMonth() + 1)).slice(-2) +
+    "/" +
+    MyDate.getFullYear();
+
+  var txt =
+    "Реп. зал: " +
+    req.query.temp_c +
+    String.fromCharCode(176) +
+    "C  (посл. обн.: " +
+    MyDateString +
+    ")";
+
+  req.app.set("temp_droom", txt);
+  db = req.app.get("db");
+
+  db.getItem("droom").then(a => {
+    if (typeof a === "undefined") {
+      db.setItem("droom", new Array());
+    }
+    //console.log("Before: ");
+    //console.log(a);
+    if (a.length > 500) {
+      a.shift();
+    }
+    a.push({ temp: req.query.temp_c, timestamp: MyDateString });
+    db.updateItem("droom", a);
     //console.log("After: ");
     //console.log(a);
     res.send("ok");
